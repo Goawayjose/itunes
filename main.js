c55680328ee093864549f2997cb8d0f8f05f6274
Jose Ramirez Imac <Jrramirez1994@gmail.com>
2017-03-22 10:01:56 -0700

    Hardcoding bubbles
    yes here we are actually hard coding bubbles, some images still need to
    be added on

--- main.js
+++ main.js
@@ -3,51 +3,77 @@ var app = new Vue({
   data: {
     songs: {
       song1: {
-        id: '100',
+        id: '75',
         title: 'Famous',
         artist: 'Kanye West',
         cover: 'img/pablo.jpg',
-        trackMp3: 'tunes/Famous.mp3'
+        trackMp3: 'tunes/Famous.mp3',
+        leftNom: '283',
+        topNom: '442'
       },
       song2: {
         id: '40',
         title: 'Let it Happen',
         artist: 'Tame Impala',
         cover: 'img/currents.jpg',
-        trackMp3: 'tunes/Let_It_Happen.mp3'
+        trackMp3: 'tunes/Let_It_Happen.mp3',
+        leftNom: '30',
+        topNom: '454'
       },
       song3: {
-        id: '400',
-        title: 'Let it Happen',
-        artist: 'Tame Impala',
-        cover: 'img/currents.jpg',
-        trackMp3: 'tunes/Let_It_Happen.mp3'
+        id: '300',
+        title: '24k Magic',
+        artist: 'Bruno Mars',
+        cover: 'img/24k.png',
+        trackMp3: 'tunes/Let_It_Happen.mp3',
+        leftNom: '20',
+        topNom: '450'
+      },
+      song4: {
+        id: '100',
+        title: 'Both',
+        artist: 'Gucci Mane ft. Drake',
+        cover: 'img/gucciBoth.jpg',
+        trackMp3: 'tunes/Let_It_Happen.mp3',
+        leftNom: '205',
+        topNom: '364'
+      },
+      song5: {
+        id: '105',
+        title: 'Goosebumps',
+        artist: 'Travis Scott ft. Kendrick Lamar',
+        cover: 'img/goosebumbs.jpg',
+        trackMp3: 'tunes/Let_It_Happen.mp3',
+        leftNom: '168',
+        topNom: '155'
+      },
+      song6: {
+        id: '200',
+        title: 'Fake Love',
+        artist: 'Drake',
+        cover: 'img/moreLife.jpg',
+        trackMp3: 'tunes/Let_It_Happen.mp3',
+        leftNom: '220',
+        topNom: '-15'
+      },
+      song7: {
+        id: '220',
+        title: 'Starboy',
+        artist: 'The Weeknd',
+        cover: 'img/Starboy.png',
+        trackMp3: 'tunes/Let_It_Happen.mp3',
+        leftNom: '0',
+        topNom: '225'
       }
-    },
-    song: {
-
     }
   }
 });
 
 
-var $newdiv = $('.circle-btn-xsSmall');
-var $newdivSiblings = $('.circle-btn-xsSmall').parent().siblings().children();
+$('.circleBtn').click(function(){
+  $(this).children().addClass('show');
+});
 
-
-var posx = (Math.random() * ($(document).width())).toFixed();
-var posy = (Math.random() * ($(document).height())).toFixed();
-
-
-if($newdivSiblings.css('height') === 'auto'){}
-   $newdiv.css({
-       'position':'absolute',
-       'left':posx+'px',
-       'top':posy+'px'
-     });
-
-
-
-console.log(app.$data.songs);
-
-console.log(app.$data.song);
+$('.blackback').click(function(){
+  $('.audioWrap.show').toggleClass('show');
+});
