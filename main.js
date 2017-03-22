90eb2966980550b63e2910969462f6cb6c75ee9a
Jose Ramirez <jrramirez1994@gmail.com>
2017-03-21 20:14:07 -0700

    got some randomness working

--- main.js
+++ main.js
@@ -3,15 +3,24 @@ var app = new Vue({
   data: {
     songs: {
       song1: {
-        id: 1,
+        id: '100',
         title: 'Famous',
         artist: 'Kanye West',
+        cover: 'img/pablo.jpg',
         trackMp3: 'tunes/Famous.mp3'
       },
       song2: {
-        id: 2,
+        id: '40',
         title: 'Let it Happen',
         artist: 'Tame Impala',
+        cover: 'img/currents.jpg',
+        trackMp3: 'tunes/Let_It_Happen.mp3'
+      },
+      song3: {
+        id: '400',
+        title: 'Let it Happen',
+        artist: 'Tame Impala',
+        cover: 'img/currents.jpg',
         trackMp3: 'tunes/Let_It_Happen.mp3'
       }
     },
@@ -22,6 +31,23 @@ var app = new Vue({
 });
 
 
-$('#selector').change(function(){
+var $newdiv = $('.circle-btn-xsSmall');
+var $newdivSiblings = $('.circle-btn-xsSmall').parent().siblings().children();
 
-});
+
+var posx = (Math.random() * ($(document).width())).toFixed();
+var posy = (Math.random() * ($(document).height())).toFixed();
+
+
+if($newdivSiblings.css('height') === 'auto'){}
+   $newdiv.css({
+       'position':'absolute',
+       'left':posx+'px',
+       'top':posy+'px'
+     });
+
+
+
+console.log(app.$data.songs);
+
+console.log(app.$data.song);
