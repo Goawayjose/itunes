092718719b56e81f7550de7506efd072312b67f6
Jose Ramirez <jrramirez1994@gmail.com>
2017-03-24 18:45:09 -0700

    terrible day but still working

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ var app = new Vue({
         artist: 'Tame Impala',
         cover: 'img/currents.jpg',
         trackMp3: 'tunes/Let_It_Happen.mp3',
-        leftNom: '30',
-        topNom: '454'
+        leftNom: '120',
+        topNom: '184'
       },
       song3: {
         id: '300',
@@ -64,10 +64,84 @@ var app = new Vue({
         trackMp3: 'tunes/Let_It_Happen.mp3',
         leftNom: '0',
         topNom: '225'
+      },
+      song8: {
+        id: '170',
+        title: 'Bad and boujee',
+        artist: 'Migos',
+        cover: 'img/badnboujee.jpg',
+        trackMp3: 'tunes/Let_It_Happen.mp3',
+        leftNom: '0',
+        topNom: '25'
+      },
+      song9: {
+        id: '60',
+        title: 'Bad and boujee',
+        artist: 'Migos',
+        cover: 'img/broccoli.jpg',
+        trackMp3: 'tunes/Let_It_Happen.mp3',
+        leftNom: '0',
+        topNom: '185'
+      },
+      song10: {
+        id: '134',
+        title: 'Caroline',
+        artist: 'Amine',
+        cover: 'img/CarolineAmine.jpg',
+        trackMp3: 'tunes/Let_It_Happen.mp3',
+        leftNom: '268',
+        topNom: '190'
+      },
+      song11: {
+        id: '68',
+        title: 'Cold Water',
+        artist: 'Major Lazer',
+        cover: 'img/coldWater.png',
+        trackMp3: 'tunes/Let_It_Happen.mp3',
+        leftNom: '224',
+        topNom: '294'
+      },
+      song12: {
+        id: '68',
+        title: 'Cold Water',
+        artist: 'Major Lazer',
+        cover: 'img/coldWater.png',
+        trackMp3: 'tunes/Let_It_Happen.mp3',
+        leftNom: '224',
+        topNom: '294'
+      },
+      song13: {
+        id: '154',
+        title: 'EveryDay Ft. Future',
+        artist: 'Ariana Grande',
+        cover: 'img/dangerousWomen.png',
+        trackMp3: 'tunes/Let_It_Happen.mp3',
+        leftNom: '327',
+        topNom: '494'
+      },
+      song14: {
+        id: '270',
+        title: 'Bounce Back',
+        artist: 'Big Sean',
+        cover: 'img/iDecided.jpg',
+        trackMp3: 'tunes/Let_It_Happen.mp3',
+        leftNom: '377',
+        topNom: '244'
+      },
+      song15: {
+        id: '112',
+        title: 'Moves',
+        artist: 'Big Sean',
+        cover: 'img/iDecided.jpg',
+        trackMp3: 'tunes/Let_It_Happen.mp3',
+        leftNom: '327',
+        topNom: '64'
       }
     }
   }
 });
+
+
 
 
 $('.circleBtn').click(function(){
